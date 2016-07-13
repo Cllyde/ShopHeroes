5bfb4e233f94f96701ef44c6a95f0f86da45cba0
Cllyde <cllyde_soldat@yahoo.com>
2016-07-13 16:47:17 -0500

    Initial commit.

---
+++ WorkerSkills/Controllers/WorkersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WorkerSkills.Models;
+
+namespace WorkerSkills.Controllers
+{
+    public class WorkersController : Controller
+    {
+        static ShopHeroesEntities.ShopHeroesModel db = new ShopHeroesEntities.ShopHeroesModel();
+        // GET: Workers
+        public ActionResult Index()
+        {
+            var allWorkers = db.Workers.Include("Skills").ToList();
+            var workerSkillGridViewModel = new WorkerSkillGridViewModel(allWorkers);
+            return View(workerSkillGridViewModel);
+        }
+    }
+}
---
+++ WorkerSkills/Models/WorkerSkillGridViewModel.cs
@@ -0,0 +1,34 @@
+﻿using ShopHeroesEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorkerSkills.Models
+{
+    public class WorkerSkillGridViewModel
+    {
+        const int GROUP_COUNT = 4;
+
+        public List<List<Worker>> WorkerGroups { get; set; }
+
+        public WorkerSkillGridViewModel(List<Worker> workerList)
+        {
+            WorkerGroups = new List<List<Worker>>();
+            var currentGroup = new List<Worker>();
+            for (int i = 0; i < workerList.Count; i++)
+            {
+                if (i % GROUP_COUNT == 0)
+                {
+                    WorkerGroups.Add(currentGroup);
+                    currentGroup = new List<Worker>();
+                    currentGroup.Add(workerList[i]);
+                }
+                else
+                {
+                    currentGroup.Add(workerList[i]);
+                }
+            }
+        }
+    }
+}
