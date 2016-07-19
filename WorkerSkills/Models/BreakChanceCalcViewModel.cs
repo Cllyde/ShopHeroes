ddeefb5d174abe6bc4ad14a8a28ff3be99f44dd7
Cllyde <cllyde_soldat@yahoo.com>
2016-07-19 09:28:46 -0500

    Including bootstrap update and new BreakChanceCalcViewModel

---
+++ WorkerSkills/Models/BreakChanceCalcViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorkerSkills.Models
+{
+    public class BreakChanceCalcViewModel
+    {
+        public int HeroLevel { get; set; }
+
+        public int ItemLevel { get; set; }
+
+        public double AffinityModifier { get; set; }
+
+        public double QualityModifier { get; set; }
+    }
+}
